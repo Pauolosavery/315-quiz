9e6c84e69690180158b8b7a0d794fb57de98a75c
Damir <demchikbig777@mail.ru>
2022-07-21 18:44:32 +0300

    Game+ done

---
+++ client/src/redux/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux'
+// import photoReducer from './photoReducer'
+// import userReducer from './userReducer'
+
+
+const rootReducer = combineReducers({
+//  photos:photoReducer,
+//  user:userReducer
+})
+
+export default rootReducer
